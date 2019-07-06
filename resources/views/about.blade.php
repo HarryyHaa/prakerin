7586cb9492c775865528d4d8d6d51f49e3d4abc3
HarryyHaa <harryya120@gmail.com>
2019-07-06 13:48:59 +0700

    templete

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -139,7 +139,7 @@
     <!-- ##### Header Area End ##### -->
 
     <!-- ##### Breadcrumb Area Start ##### -->
-    <section class="breadcrumb-area bg-img bg-overlay" style="background-image: url({{ asset('assets/frontend/img/bg-img/40.jpg')}}')">
+    <section class="breadcrumb-area bg-img bg-overlay" style="background-image: url({{ asset('assets/frontend/img/bg-img/40.jpg')}})">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-12">
